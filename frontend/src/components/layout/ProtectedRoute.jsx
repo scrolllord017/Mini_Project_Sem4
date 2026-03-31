ec6b6c8a5089b23b1d98d4297ea2bda5d7e32593
Ayush Sharma <ayushsharma120904@gmail.com>
2026-03-31 13:27:11 +0530

    protected route push

---
+++ frontend/src/components/layout/ProtectedRoute.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Navigate, useLocation } from 'react-router-dom';
+import useAuthStore from '../../store/useAuthStore';
+
+export default function ProtectedRoute({ children }) {
+  const { isAuthenticated } = useAuthStore();
+  const location = useLocation();
+
+  if (!isAuthenticated) {
+    return <Navigate to="/login" state={{ from: location }} replace />;
+  }
+
+  return children;
+}
